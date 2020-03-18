96a417e24d4e4a9ceb4bac03516922defaaa3845
Bartek <barpor90@gmail.com>
2020-03-18 01:25:58 +0100

    japierdole

--- app/src/main/java/com/lvddr/coronacounter/MainActivity.kt
+++ app/src/main/java/com/lvddr/coronacounter/MainActivity.kt
@@ -1,36 +1,46 @@
 package com.lvddr.coronacounter
 
+import android.os.AsyncTask
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.lvddr.coronacounter.R.layout.activity_main
-import kotlinx.android.synthetic.main.activity_main.*
 import org.jsoup.Jsoup
-
 
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
-        val thread = FetchStat()
-        thread.start()
+        val task = Async(this)
+        task.execute()
+
     }
-    inner class FetchStat: Thread() {
-        override fun run() {
+
+
+
+    inner class Async internal constructor(context: MainActivity) : AsyncTask<Void, Void, String>(){
+
+
+        override fun doInBackground(vararg params: Void): String? {
+
             val url = "https://www.worldometers.info/coronavirus/"
             val doc1 = Jsoup.connect(url).get()
             val cases = doc1.select("#maincounter-wrap > div > span").first().text()
             val dead = doc1.select("#maincounter-wrap > div > span").get(1).text()
             val recovered = doc1.select("#maincounter-wrap > div > span").get(2).text()
-            runOnUiThread() {
-                findViewById<TextView>(R.id.textView).text = cases
-                findViewById<TextView>(R.id.textView3).text = dead
-                findViewById<TextView>(R.id.textView4).text = recovered
+            println(cases)
+            println(dead)
+            println(recovered)
+            return null
             }
+
+        override fun onPostExecute(result: String?) {
+            findViewById<TextView>(R.id.textView).text = cases
         }
+
 
     }
 }
