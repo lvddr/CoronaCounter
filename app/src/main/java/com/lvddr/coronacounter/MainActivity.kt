af8b66a231b9be07b4534356625378dcd6a02b69
Bartek <barpor90@gmail.com>
2020-03-27 11:54:35 +0100

    fuck this

--- app/src/main/java/com/lvddr/coronacounter/MainActivity.kt
+++ app/src/main/java/com/lvddr/coronacounter/MainActivity.kt
@@ -1,61 +1,88 @@
 package com.lvddr.coronacounter
 
-import android.content.Intent
+import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.lvddr.coronacounter.R.layout.activity_main
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.textView
 import kotlinx.android.synthetic.main.poland.*
 import org.jsoup.Jsoup
 
-
 class MainActivity : AppCompatActivity() {
+    var context: Context = this@MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(activity_main)
-        val task = Async(this)
-        task.execute()
-        gtpl.setOnClickListener() {
-            val intent = Intent(this, Poland::class.java)
-            startActivity(intent)
+        setContentView(R.layout.activity_main)
+        val glist = GetList(this)
+        glist.execute()
+        val submitBtn: Button = findViewById(R.id.button2)
+        submitBtn.setOnClickListener() {
+            val gcountry = GetCountry(this)
+            gcountry.execute()
         }
     }
 
-    inner class Async internal constructor(context: MainActivity) : AsyncTask<Void, Void, String>(){
+
+    inner class GetCountry internal constructor(context: MainActivity) : AsyncTask<Void?, Void?, String?>() {
+        var cases: String? = null
+        var dead: String? = null
+        var recovered: String? = null
+        override fun onPreExecute() {
+            textView.text = "Loading"
+            textView3.text = ""
+            textView4.text = ""
+        }
+
+        override fun doInBackground(vararg params: Void?): String? {
+            val docc = Jsoup.connect("https://www.worldometers.info/coronavirus/" + spinner.selectedItem.toString()).followRedirects(true).get()
+            cases = docc.select("#maincounter-wrap > div > span").get(0).text()
+            dead = docc.select("#maincounter-wrap > div > span").get(1).text()
+            recovered = docc.select("#maincounter-wrap > div > span").get(2).text()
+            println(cases)
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            cases?.let { findViewById<TextView>(R.id.textView).text = it }
+            dead?.let { findViewById<TextView>(R.id.textView3).text = it }
+            recovered?.let { findViewById<TextView>(R.id.textView4).text = it }
+        }
+
+    }
+    inner class GetList internal constructor(context: MainActivity) : AsyncTask<Void, Void, List<String>>() {
         var cases: String? = null
         var dead: String? = null
         var recovered: String? = null
 
         override fun onPreExecute() {
-            textView.text = getString(R.string.loading)
+            textView.text = "Loading"
+            Toast.makeText(this@MainActivity, "Currently loading worldwide stats", Toast.LENGTH_LONG).show()
         }
 
-        override fun doInBackground(vararg params: Void): String? {
-
-            val url = "https://www.worldometers.info/coronavirus/"
-            val doc1 = Jsoup.connect(url).get()
-            cases = doc1.select("#maincounter-wrap > div > span").first().text()
-            dead = doc1.select("#maincounter-wrap > div > span").get(1).text()
-            recovered = doc1.select("#maincounter-wrap > div > span").get(2).text()
-
-            return null
+        override fun doInBackground(vararg params: Void?): List<String> {
+            val cipa = Jsoup.connect("https://www.worldometers.info/coronavirus").get()
+            val a = cipa.select("#main_table_countries_today").first()
+            val b = a.select("a[href].mt_a").map { it.attr("href") }
+            cases = cipa.select("#maincounter-wrap > div > span").get(0).text()
+            dead = cipa.select("#maincounter-wrap > div > span").get(1).text()
+            recovered = cipa.select("#maincounter-wrap > div > span").get(2).text()
+            return b
         }
 
-        override fun onPostExecute(result: String?) {
-            cases?.let { findViewById<TextView>(R.id.textView).text = it}
-            dead?.let { findViewById<TextView>(R.id.textView3).text = it}
-            recovered?.let { findViewById<TextView>(R.id.textView4).text = it}
+        override fun onPostExecute(result: List<String>) {
+            val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, result)
+            spinner.setAdapter(adapter)
+            textView.text = "Ready"
+            cases?.let { findViewById<TextView>(R.id.textView).text = it }
+            dead?.let { findViewById<TextView>(R.id.textView3).text = it }
+            recovered?.let { findViewById<TextView>(R.id.textView4).text = it }
+
+
         }
-
-
     }
 }
-
-
-
 
